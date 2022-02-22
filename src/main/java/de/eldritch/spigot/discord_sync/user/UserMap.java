5c860cee928f2958a50e4d1dc7074d111c424a42
TurtleException <turtle.exception@eldritch.de>
2022-02-22 01:51:28 +0100

    Simplify UserMap indexing

--- src/main/java/de/eldritch/spigot/discord_sync/user/UserMap.java
+++ src/main/java/de/eldritch/spigot/discord_sync/user/UserMap.java
@@ -8,33 +8,22 @@ import java.util.HashMap;
 import java.util.UUID;
 
 final class UserMap {
-    private final HashMap<Long, User> users = new HashMap<>();
-    private long index = 0;
-
-    private final HashMap<UUID, Long> uuidIndex      = new HashMap<>();
-    private final HashMap<Long, Long> snowflakeIndex = new HashMap<>();
-
-    /* ------------------------- */
-
-    private @Nullable User getByIndex(long index) {
-        return users.get(index);
-    }
+    private final HashMap<UUID, User> uuidIndex      = new HashMap<>();
+    private final HashMap<Long, User> snowflakeIndex = new HashMap<>();
 
     /* ------------------------- */
 
     public User get(@NotNull UUID uuid) {
-        return this.getByIndex(uuidIndex.get(uuid));
+        return uuidIndex.get(uuid);
     }
 
     public User get(long snowflake) {
-        return this.getByIndex(snowflakeIndex.get(snowflake));
+        return snowflakeIndex.get(snowflake);
     }
 
     public void remove(@NotNull User user) {
-        if (!users.containsValue(user)) return;
-
+        uuidIndex.remove(user.uuid());
         snowflakeIndex.remove(user.snowflake());
-        users.remove(uuidIndex.remove(user.uuid()), user);
     }
 
     /* ------------------------- */
@@ -45,22 +34,14 @@ final class UserMap {
      * @return true if the User's UUID is new to this map.
      */
     public boolean put(@NotNull User user) {
-        long old   = uuidIndex.getOrDefault(user.uuid(), -1L);
-        long index = old >= 0 ? old : this.index++;
-
-        users.put(index, user);
-        if (old != index) {
-            uuidIndex.put(user.uuid(), index);
-            if (user.isDiscordConnected())
-                snowflakeIndex.put(user.snowflake(), index);
-        }
-
-        return old != index;
+        if (user.isDiscordConnected())
+            snowflakeIndex.put(user.snowflake(), user);
+        return uuidIndex.put(user.uuid(), user) != null;
     }
 
     /* ------------------------- */
 
     public Collection<User> getUserView() {
-        return users.values();
+        return uuidIndex.values();
     }
 }
