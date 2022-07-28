cdde295ee47651d2a3be60184ee0a4067cec81e7
TurtleException <turtle.exception@eldritch.de>
2022-07-28 03:26:39 +0200

    Create plugin base files

---
+++ src/main/java/de/turtle_exception/spigot/DiscordSync.java
@@ -0,0 +1,15 @@
+package de.turtle_exception.spigot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * The plugin main class.
+ */
+@SuppressWarnings("unused")
+public class DiscordSync extends JavaPlugin {
+    public static DiscordSync plugin;
+
+    public DiscordSync() {
+        plugin = this;
+    }
+}
