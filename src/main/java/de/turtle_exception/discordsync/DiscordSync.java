c49a470255ae4c1cc80332614de074ace66b8370
TurtleException <turtle.exception@eldritch.de>
2022-12-15 23:45:35 +0100

    Create plugin main class

---
+++ src/main/java/de/turtle_exception/discordsync/DiscordSync.java
@@ -0,0 +1,19 @@
+package de.turtle_exception.discordsync;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DiscordSync extends JavaPlugin {
+    public DiscordSync() {
+
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
