6342d467c22178c766b0e1ec5c22c484a986fdd9
TurtleException <turtle.exception@eldritch.de>
2022-12-18 02:04:38 +0100

    Create SyncUser

---
+++ src/main/java/de/turtle_exception/discordsync/SyncUser.java
@@ -0,0 +1,33 @@
+package de.turtle_exception.discordsync;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class SyncUser implements Entity {
+    private final long id;
+
+    private final ArrayList<UUID> minecraftIds;
+    private final ArrayList<Long>   discordIds;
+
+    SyncUser(long id, List<UUID> minecraftIds, List<Long> discordIds) {
+        this.id = id;
+        this.minecraftIds = new ArrayList<>(minecraftIds);
+        this.discordIds   = new ArrayList<>(discordIds);
+    }
+
+    @Override
+    public long getId() {
+        return this.id;
+    }
+
+    public @NotNull List<UUID> getMinecraftIds() {
+        return List.copyOf(minecraftIds);
+    }
+
+    public @NotNull List<Long> getDiscordIds() {
+        return List.copyOf(discordIds);
+    }
+}
