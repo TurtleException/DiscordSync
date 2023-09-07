465a42a12f0c5d51312f9a9d0131710c5dc0f30c
TurtleException <turtle.exception@eldritch.de>
2023-09-08 01:04:34 +0200

    Create default configs

--- src/main/java/de/turtleboi/spigot/dsync/DiscordSync.java
+++ src/main/java/de/turtleboi/spigot/dsync/DiscordSync.java
@@ -3,5 +3,11 @@ package de.turtleboi.spigot.dsync;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DiscordSync extends JavaPlugin {
+    public DiscordSync() { }
 
+    @Override
+    public void onEnable() {
+        this.saveResource("config.yml", false);
+        this.saveResource("users.yml", false);
+    }
 }
