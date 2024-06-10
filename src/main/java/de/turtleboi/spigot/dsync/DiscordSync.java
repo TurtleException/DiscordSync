3f0d26a3c48c621552d1c9a98d04d9db12a1aacf
TurtleException <turtle.exception@eldritch.de>
2024-06-10 20:32:52 +0200

    Remove plugin singleton

--- src/main/java/de/turtleboi/spigot/dsync/DiscordSync.java
+++ src/main/java/de/turtleboi/spigot/dsync/DiscordSync.java
@@ -4,12 +4,6 @@ import de.turtleboi.spigot.dsync.util.JDALogFilter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DiscordSync extends JavaPlugin {
-    public static DiscordSync plugin;
-
-    public DiscordSync() {
-        plugin = this;
-    }
-
     @Override
     public void onEnable() {
         this.saveResource("config.yml", false);
