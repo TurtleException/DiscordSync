aedbdd7849e7e0f4f1e617396b6551dd0c344d1d
TurtleException <turtle.exception@eldritch.de>
2024-06-10 20:34:14 +0200

    Implement UserRegistry

--- src/main/java/de/turtleboi/spigot/dsync/DiscordSync.java
+++ src/main/java/de/turtleboi/spigot/dsync/DiscordSync.java
@@ -1,15 +1,25 @@
 package de.turtleboi.spigot.dsync;
 
+import de.turtleboi.spigot.dsync.user.UserRegistry;
 import de.turtleboi.spigot.dsync.util.JDALogFilter;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 public class DiscordSync extends JavaPlugin {
+    private UserRegistry userRegistry;
+
     @Override
     public void onEnable() {
         this.saveResource("config.yml", false);
         this.saveResource("users.yml", false);
 
+        this.userRegistry = new UserRegistry();
+
         JDALogFilter jdaLogFilter = new JDALogFilter(this);
         jdaLogFilter.start();
+    }
+
+    public @NotNull UserRegistry getUserRegistry() {
+        return this.userRegistry;
     }
 }
