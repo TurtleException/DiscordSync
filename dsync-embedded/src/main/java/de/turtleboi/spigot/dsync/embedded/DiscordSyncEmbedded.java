95a7ec6d308c1fe639d9e13ede5c3e37943e0401
TurtleException <turtle.exception@eldritch.de>
2024-07-27 14:15:10 +0200

    Implement UserManager in embedded module

--- dsync-embedded/src/main/java/de/turtleboi/spigot/dsync/embedded/DiscordSyncEmbedded.java
+++ dsync-embedded/src/main/java/de/turtleboi/spigot/dsync/embedded/DiscordSyncEmbedded.java
@@ -1,16 +1,32 @@
 package de.turtleboi.spigot.dsync.embedded;
 
 import de.turtleboi.spigot.dsync.api.ObeliskAPI;
+import de.turtleboi.spigot.dsync.api.entity.dao.UserDAO;
+import de.turtleboi.spigot.dsync.embedded.user.UserManager;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
 
 public class DiscordSyncEmbedded extends JavaPlugin implements ObeliskAPI {
+    private UserManager userManager;
+
     @Override
     public void onEnable() {
-        this.saveResource("users.yml", false);
+        this.saveResource("users.yaml", false);
+
+        YamlConfiguration userConfig = YamlConfiguration.loadConfiguration(new File(this.getDataFolder(), "users.yaml"));
+        this.userManager = new UserManager(this, userConfig);
 
         JDALogFilter jdaLogFilter = new JDALogFilter(this);
         jdaLogFilter.start();
 
         ObeliskAPI.register(this);
+    }
+
+    @Override
+    public @NotNull UserDAO getUserDAO() {
+        return this.userManager;
     }
 }
---
+++ dsync-embedded/src/main/java/de/turtleboi/spigot/dsync/embedded/user/UserManager.java
@@ -0,0 +1,310 @@
+package de.turtleboi.spigot.dsync.embedded.user;
+
+import de.turtleboi.spigot.dsync.api.entity.User;
+import de.turtleboi.spigot.dsync.api.entity.dao.UserDAO;
+import de.turtleboi.spigot.dsync.embedded.DiscordSyncEmbedded;
+import de.turtleboi.spigot.dsync.util.CloseableLock;
+import de.turtleboi.spigot.dsync.util.IdUtil;
+import de.turtleboi.spigot.dsync.util.ReadWriteCloseableLock;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.logging.Level;
+
+public class UserManager implements UserDAO {
+    private final DiscordSyncEmbedded plugin;
+    private final YamlConfiguration config;
+
+    private final ReadWriteCloseableLock lock = new ReadWriteCloseableLock();
+
+    public UserManager(@NotNull DiscordSyncEmbedded plugin, @NotNull YamlConfiguration userConfig) {
+        this.plugin = plugin;
+        this.config = userConfig;
+    }
+
+    @Override
+    public @NotNull User createUser(long id, @NotNull String name) {
+        try (CloseableLock ignored = this.lock.write()) {
+            ConfigurationSection section = config.createSection(String.valueOf(id));
+
+            section.set("name", name);
+        }
+
+        return new User(this.plugin, id);
+    }
+
+    public @NotNull User createUser(@NotNull String name) {
+        return this.createUser(IdUtil.newId(((byte) "dsync-user".hashCode())), name);
+    }
+
+    @Override
+    public @NotNull User provideUserByDiscord(long snowflake, @NotNull String name) {
+        try (CloseableLock ignored = this.lock.read()) {
+            return this.getUserByDiscordId(snowflake).orElseGet(() -> {
+                // no user has claimed the discord id yet, create a new one
+                try (CloseableLock ignored1 = this.lock.write()) {
+                    User user = this.createUser(name);
+                    user.addDiscordAccount(snowflake);
+                    return user;
+                }
+            });
+        }
+    }
+
+    @Override
+    public @NotNull User provideUserByMinecraft(@NotNull UUID uuid, @NotNull String name) {
+        try (CloseableLock ignored = this.lock.read()) {
+            return this.getUserByMinecraftId(uuid).orElseGet(() -> {
+                // no user has claimed the discord id yet, create a new one
+                try (CloseableLock ignored1 = this.lock.write()) {
+                    User user = this.createUser(name);
+                    user.addMinecraftAccount(uuid);
+                    return user;
+                }
+            });
+        }
+    }
+
+    @Override
+    public @NotNull List<User> listUsers() {
+        List<User> users = new ArrayList<>();
+
+        try (CloseableLock ignored = this.lock.read()) {
+            for (String key : this.config.getKeys(false)) {
+                try {
+                    long id = Long.parseLong(key);
+
+                    users.add(new User(this.plugin, id));
+                } catch (NumberFormatException e) {
+                    this.plugin.getLogger().log(Level.WARNING, "Invalid key in user config: " + key);
+                }
+            }
+        }
+
+        return users;
+    }
+
+    @Override
+    public @NotNull Optional<User> getUser(long id) {
+        String idStr = String.valueOf(id);
+
+        try (CloseableLock ignored = this.lock.read()) {
+            if (this.config.contains(idStr))
+                return Optional.of(new User(this.plugin, id));
+        }
+
+        return Optional.empty();
+    }
+
+    @Override
+    public @NotNull Optional<User> getUserByDiscordId(long snowflake) {
+        try (CloseableLock ignored = this.lock.read()) {
+            for (String key : this.config.getKeys(false)) {
+                List<Long> snowflakes = this.config.getLongList(key + ".discord");
+
+                if (snowflakes.contains(snowflake)) {
+                    try {
+                        long id = Long.parseLong(key);
+
+                        return Optional.of(new User(this.plugin, id));
+                    } catch (NumberFormatException e) {
+                        this.plugin.getLogger().log(Level.WARNING, "Invalid key in user config: " + key);
+
+                        // ignore invalid keys and continue iterating
+                    }
+                }
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    @Override
+    public @NotNull Optional<User> getUserByMinecraftId(@NotNull UUID uuid) {
+        String uuidStr = uuid.toString();
+
+        try (CloseableLock ignored = this.lock.read()) {
+            for (String key : this.config.getKeys(false)) {
+                List<String> uuids = this.config.getStringList(key + ".minecraft");
+
+                if (uuids.contains(uuidStr)) {
+                    try {
+                        long id = Long.parseLong(key);
+
+                        return Optional.of(new User(this.plugin, id));
+                    } catch (NumberFormatException e) {
+                        this.plugin.getLogger().log(Level.WARNING, "Invalid key in user config: " + key);
+
+                        // ignore invalid keys and continue iterating
+                    }
+                }
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    @Override
+    public @NotNull String getUserName(long id) {
+        String idStr = String.valueOf(id);
+
+        try (CloseableLock ignored = this.lock.read()) {
+            ConfigurationSection section = this.config.getConfigurationSection(idStr);
+
+            if (section != null) {
+                String name = section.getString("name");
+
+                if (name != null)
+                    return name;
+            }
+        }
+
+        // TODO: replace with custom exception
+        throw new Error("Not implemented");
+    }
+
+    @Override
+    public @NotNull List<Long> getUserDiscordAccounts(long id) {
+        String idStr = String.valueOf(id);
+
+        try (CloseableLock ignored = this.lock.read()) {
+            ConfigurationSection section = this.config.getConfigurationSection(idStr);
+
+            if (section != null)
+                return section.getLongList("discord");
+        }
+
+        // TODO: replace with custom exception
+        throw new Error("Not implemented");
+    }
+
+    @Override
+    public @NotNull List<UUID> getUserMinecraftAccounts(long id) {
+        String idStr = String.valueOf(id);
+
+        try (CloseableLock ignored = this.lock.read()) {
+            ConfigurationSection section = this.config.getConfigurationSection(idStr);
+
+            if (section != null) {
+                List<String> uuidStrings = section.getStringList("minecraft");
+                List<UUID>   uuids       = new ArrayList<>(uuidStrings.size());
+
+                for (String uuidStr : uuidStrings) {
+                    try {
+                        uuids.add(UUID.fromString(uuidStr));
+                    } catch (IllegalArgumentException e) {
+                        this.plugin.getLogger().log(Level.WARNING, "Invalid UUID in " + idStr + ".minecraft: " + uuidStr);
+
+                        // skip invalid UUIDs and continue iterating
+                    }
+                }
+
+                return uuids;
+            }
+        }
+
+        // TODO: replace with custom exception
+        throw new Error("Not implemented");
+    }
+
+    @Override
+    public void setUserName(long id, @NotNull String name) {
+        String idStr = String.valueOf(id);
+
+        try (CloseableLock ignored = this.lock.write()) {
+            ConfigurationSection section = this.config.getConfigurationSection(idStr);
+
+            // TODO: replace with custom exception
+            if (section == null)
+                throw new Error("Not implemented");
+
+            section.set("name", name);
+        }
+    }
+
+    @Override
+    public void addUserDiscordAccount(long id, long snowflake) {
+        String idStr = String.valueOf(id);
+
+        try (CloseableLock ignored = this.lock.write()) {
+            ConfigurationSection section = this.config.getConfigurationSection(idStr);
+
+            // TODO: replace with custom exception
+            if (section == null)
+                throw new Error("Not implemented");
+
+            List<Long> snowflakes = section.getLongList("discord");
+            snowflakes.add(snowflake);
+
+            section.set("discord", snowflakes);
+        }
+    }
+
+    @Override
+    public void removeUserDiscordAccount(long id, long snowflake) {
+        String idStr = String.valueOf(id);
+
+        try (CloseableLock ignored = this.lock.write()) {
+            ConfigurationSection section = this.config.getConfigurationSection(idStr);
+
+            // TODO: replace with custom exception
+            if (section == null)
+                throw new Error("Not implemented");
+
+            List<Long> snowflakes = section.getLongList("discord");
+            snowflakes.remove(snowflake);
+
+            section.set("discord", snowflakes);
+        }
+    }
+
+    @Override
+    public void addUserMinecraftAccount(long id, @NotNull UUID uuid) {
+        String idStr = String.valueOf(id);
+
+        try (CloseableLock ignored = this.lock.write()) {
+            ConfigurationSection section = this.config.getConfigurationSection(idStr);
+
+            // TODO: replace with custom exception
+            if (section == null)
+                throw new Error("Not implemented");
+
+            List<String> uuids = section.getStringList("minecraft");
+            uuids.add(uuid.toString());
+
+            section.set("minecraft", uuids);
+        }
+    }
+
+    @Override
+    public void removeUserMinecraftAccount(long id, @NotNull UUID uuid) {
+        String idStr = String.valueOf(id);
+
+        try (CloseableLock ignored = this.lock.write()) {
+            ConfigurationSection section = this.config.getConfigurationSection(idStr);
+
+            // TODO: replace with custom exception
+            if (section == null)
+                throw new Error("Not implemented");
+
+            List<String> uuids = section.getStringList("minecraft");
+            uuids.remove(uuid.toString());
+
+            section.set("minecraft", uuids);
+        }
+    }
+
+    @Override
+    public void deleteUser(long id) {
+        String idStr = String.valueOf(id);
+
+        try (CloseableLock ignored = this.lock.write()) {
+            this.config.set(idStr, null);
+        }
+    }
+}
