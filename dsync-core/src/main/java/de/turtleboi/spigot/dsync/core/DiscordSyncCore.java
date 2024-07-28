bc7d945671499c7d49bbfa321fd59cb59123082e
TurtleException <turtle.exception@eldritch.de>
2024-07-28 14:06:18 +0200

    Implement chat prefix

--- dsync-core/src/main/java/de/turtleboi/spigot/dsync/core/DiscordSyncCore.java
+++ dsync-core/src/main/java/de/turtleboi/spigot/dsync/core/DiscordSyncCore.java
@@ -3,13 +3,18 @@ package de.turtleboi.spigot.dsync.core;
 import de.turtleboi.spigot.dsync.api.DiscordSyncAPI;
 import de.turtleboi.spigot.dsync.api.chat.MessageRouter;
 import de.turtleboi.spigot.dsync.core.chat.MessageDispatcher;
+import de.turtleboi.spigot.dsync.core.command.PluginCommand;
 import de.turtleboi.spigot.dsync.core.listener.ChatInterceptor;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.logging.Level;
 
 public class DiscordSyncCore extends JavaPlugin {
     private MessageDispatcher dispatcher;
+    private TextComponent chatPrefix;
 
     @Override
     public void onEnable() {
@@ -22,6 +27,15 @@ public class DiscordSyncCore extends JavaPlugin {
         }
 
         this.saveResource("config.yml", false);
+
+        String chatPrefixStr = this.getConfig().getString("chatPrefix", "§8[§6DiscordSync§8]");
+        TextComponent prefixComp1 = new TextComponent(TextComponent.fromLegacyText(chatPrefixStr));
+        TextComponent prefixComp2 = new TextComponent(" ");
+        this.chatPrefix = new TextComponent(prefixComp1, prefixComp2);
+        this.chatPrefix.setColor(ChatColor.GRAY);
+
+        // noinspection DataFlowIssue
+        this.getCommand("dsync").setExecutor(new PluginCommand(this));
 
         this.dispatcher = new MessageDispatcher(this);
         MessageRouter router = api.getMessageRouter();
@@ -38,5 +52,9 @@ public class DiscordSyncCore extends JavaPlugin {
             router.unregisterHandler(this.dispatcher);
             this.dispatcher = null;
         } catch (IllegalStateException ignored) { }
+    }
+
+    public @NotNull TextComponent getChatPrefix() {
+        return this.chatPrefix.duplicate();
     }
 }
