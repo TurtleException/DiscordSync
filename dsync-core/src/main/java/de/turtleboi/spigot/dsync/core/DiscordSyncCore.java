2988b0e9b91aa55b7c2e21fd7e8fd1fc307b7bb2
TurtleException <turtle.exception@eldritch.de>
2024-07-28 12:33:33 +0200

    Enforce plugin load order

--- dsync-core/src/main/java/de/turtleboi/spigot/dsync/core/DiscordSyncCore.java
+++ dsync-core/src/main/java/de/turtleboi/spigot/dsync/core/DiscordSyncCore.java
@@ -4,10 +4,17 @@ import de.turtleboi.spigot.dsync.api.DiscordSyncAPI;
 import de.turtleboi.spigot.dsync.core.listener.ChatInterceptor;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Level;
+
 public class DiscordSyncCore extends JavaPlugin {
     @Override
     public void onEnable() {
-        DiscordSyncAPI api = DiscordSyncAPI.getInstance();
+        try {
+            DiscordSyncAPI api = DiscordSyncAPI.getInstance();
+        } catch (IllegalStateException e) {
+            this.getLogger().log(Level.SEVERE, "It looks like no adapter is registered. DSync Core does not work as a standalone plugin!");
+            throw e;
+        }
 
         this.saveResource("config.yml", false);
 
